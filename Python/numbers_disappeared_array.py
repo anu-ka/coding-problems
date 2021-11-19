283ef9376e1a8680a870bf69ffc4eaf8a37b1c8c
Anu Nadig <ckanupama@gmail.com>
2021-11-19 00:32:23 -0800

    Solution to numbers disappeared

--- Python/numbers_disappeared_array.py
+++ Python/numbers_disappeared_array.py
@@ -1,5 +1,24 @@
 # https://leetcode.com/problems/find-all-numbers-disappeared-in-an-array/
+import pytest
+
+
 class Solution:
     def findDisappearedNumbers(self, nums: list[int]) -> list[int]:
-        
-        
+        temp_dict = {i: -1 for i in range(1, len(nums) + 1)}
+        result = []
+
+        for i in nums:
+            if i in temp_dict:
+                temp_dict[i] = i
+
+        for i in temp_dict:
+            if temp_dict[i] == -1:
+                result.append(i)
+        return result
+
+
+@pytest.mark.parametrize(
+    ("nums", "result"), [([4, 3, 2, 7, 8, 2, 3, 1], [5, 6]), ([1, 1, 1], [2, 3])]
+)
+def test_findDisappearedNumbers(nums: list[int], result: list[int]) -> None:
+    assert result == Solution().findDisappearedNumbers(nums)
